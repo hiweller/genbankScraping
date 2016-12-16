0964527fe119c395dab734f02af4023e73f3e996
Hannah Weller <hiweller@uchicago.edu>
2016-12-16 11:18:20 -0600

    Lethrinid/Sparid+outgroups supermatrix

--- doodling.py
+++ doodling.py
@@ -3,6 +3,7 @@ import csv
 import numpy as np
 from Bio import Entrez, SeqIO, AlignIO
 from Bio.Nexus import Nexus
+from Bio import Seq
 import time
 import os
 import sys
@@ -104,19 +105,96 @@ geneList = ["COI", "16S", "CytB", "RAG1", "Rhodopsin"]
 #         muscle_cline()
 
 # alphabetizing by taxon...
-for fam in familyList:
-    AFAs = glob.glob('Temp output/'+fam+'/*.afa')
+# for fam in familyList:
+#     AFAs = glob.glob('Temp output/'+fam+'/*.afa')
+#
+#     for a in AFAs:
+#         sequence = AlignIO.read(open(a, 'rU'), 'fasta')
+#         sortedSequence = [f for f in sorted(sequence, key=lambda x : " ".join(x.description.split(" ")[1:3]))]
+#
+#         saveName = a[0:(-1-3)]+'Sorted.afa'
+#         output_handle =  open(saveName, 'w')
+#         SeqIO.write(sortedSequence, output_handle, 'fasta')
 
-    for a in AFAs:
-        sequence = AlignIO.read(open(a, 'rU'), 'fasta')
-        sortedSequence = [f for f in sorted(sequence, key=lambda x : " ".join(x.description.split(" ")[1:3]))]
+## INTERMISSION ##
+# open in Mesquite + select sequences you actually want
 
-        saveName = a[0:(-1-3)]+'Sorted.afa'
-        output_handle =  open(saveName, 'w')
-        SeqIO.write(sortedSequence, output_handle, 'fasta')
+# combine the lethrinid+sparid matrices with appropriate matrices from all other families
+combined = glob.glob('NEX/COMBINED_*Simple.nex')
+addFish = glob.glob('Temp output/Simplified Nexus/*Simple.nex')
+
+# for f in combined:
+#
+#     input_handle = AlignIO.read(open(f), "nexus")
+#     records = list(input_handle)
+#
+#     # maxLen = len(input_handle[0])# max(len(record.seq) for record in records)
+#
+#     searchTerms = re.compile(r"(?=("+'|'.join(geneList)+r"))", re.IGNORECASE)
+#     geneName = re.findall(searchTerms, f)[0]
+#
+#     for fam in addFish:
+#         if geneName in fam:
+#             add_handle = AlignIO.read(open(fam), "nexus")
+#             records2 = list(add_handle)
+#             maxLen = len(input_handle[0])
+#             maxLen2 = len(add_handle[0])#max(len(record.seq) for record in records2)
+#
+#             if maxLen < maxLen2:
+#                 for sequence in input_handle:
+#                     newSeq = str(sequence.seq).ljust(maxLen2, '-')
+#                     sequence.seq = Seq.Seq(newSeq)
+#             elif maxLen2 <= maxLen:
+#                 for sequence in add_handle:
+#                     newSeq = str(sequence.seq).ljust(maxLen, '-')
+#                     sequence.seq = Seq.Seq(newSeq)
+#
+#             for line in add_handle:
+#                 input_handle.append(line)
+#
+#
+#     saveName = 'Test/'+geneName+'.nex'
+#     output_handle = open(saveName, 'w')
+#     AlignIO.write(input_handle, output_handle, "nexus")
+
+lethrinids = glob.glob('NEX/COMBINED*Renamed_Simple.nex')
+outgroups = glob.glob('Outgroups/*Simple.nex')
+
+nexi = [(j, Nexus.Nexus(j)) for j in lethrinids]
+lethrinidsCombined = Nexus.combine(nexi)
+lethrinidsCombined.write_nexus_data(filename=open('OUT/Lethrinids_combined_all.nex', 'w'))
+
+
+nexi2 = [(j, Nexus.Nexus(j)) for j in outgroups]
+outgroupsCombined = Nexus.combine(nexi2)
+outgroupsCombined.write_nexus_data(filename=open('OUT/Outgroups_combined_all.nex', 'w'))
+
+
+# combined = glob.glob('Test/*Simple.nex')
+# nexi = [(j, Nexus.Nexus(j)) for j in combined]
+# combined = Nexus.combine(nexi)
+# combined.write_nexus_data(filename=open('Test/COMBINED_All.nex', 'w'))
+            # combos = [f, fam]
+            # nexi = [(j, Nexus.Nexus(j)) for j in combos]
+            # combined = Nexus.combine(nexi)
 
 
 
+# limits just to species name
+# for f in addFish:
+#     input_handle = AlignIO.read(open(f), "nexus")
+#
+#     for i in input_handle:
+#         newName = " ".join(i.id.split(" ")[1:3])
+#         i.id = newName
+#         # i.description = newName
+#         print i.id
+#
+#     # sortedSequence = [j for j in sorted(input_handle, key=lambda x : " ".join(x.description.split(" ")[1:3]))]
+#
+#     saveName = 'Temp output/Simplified Nexus/'+f.split("/")[-1]
+#     output_handle = open(saveName, 'w')
+#     SeqIO.write(input_handle, output_handle, "nexus")
 
 # rawGBK = glob.glob('./*Raw.gbk')
 # simplified = glob.glob("NEX/Lethrinidae*Simple.nex")
